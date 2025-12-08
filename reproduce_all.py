aa4d4669898aa73f317a37f34cc543e2c4eb490a
amittha1 <amittha@g.ucla.edu>
2025-12-07 20:48:35 -0800

    stage 8 + readme updates

--- reproduce_all.py
+++ reproduce_all.py
@@ -4,15 +4,17 @@ from src.experiments_stage4 import run_stage4
 from src.experiments_stage5 import run_stage5
 from src.experiments_stage6 import run_stage6
 from src.experiments_stage7 import run_stage7
+from src.experiments_stage8 import run_stage8
 
 def main():
-    run_stage1()
-    run_stage2()
-    run_stage3()
-    run_stage4()
-    run_stage5()
-    run_stage6()
-    run_stage7()
+    #run_stage1()
+    #run_stage2()
+    #run_stage3()
+    #run_stage4()
+    #un_stage5()
+    #run_stage6()
+    #run_stage7()
+    run_stage8()
 
 if __name__ == "__main__":
     main()
